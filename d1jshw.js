087dd03712f26881f6ef966cc34e7a10d3f95ee4
HamiltonWhite1 <92882456+HamiltonWhite1@users.noreply.github.com>
2022-02-21 17:44:21 -0500

    Day 1 JS homework for Coding Temple

---
+++ d1jshw.js
@@ -0,0 +1,36 @@
+//exercise #1
+
+let dogString = "Hello Max, my name is Dog, and I have purple eyes!"
+let dogNames = ["Max", "HAS", "Purple", "dog"]
+
+var findDogs = (dogName, listOfNames) => {
+    ourDogList = dogName.toLowerCase().split(" ")
+    ourListOfNames = listOfNames.join().toLowerCase()
+    for (var i = 0; i < ourDogList.length; i++){
+        if (ourListOfNames.includes(ourDogList[i].replace(",", ""))){
+            console.log(`${ourDogList[i].replace(",","")} is in our list`)
+        }
+        else {
+            console.log("Not match!")
+        }
+    }
+}
+findDogs(dogString, dogNames)
+
+//exercise #2
+
+let givenArr = ["Max", "Baseball", "Reboot", "Goku", "Trucks", "Rodger"]
+
+var replaceEvens = anArray => {
+    let newArr = []
+    for (var i = 0; i < anArray.length; i++){
+        if (i % 2 === 0) {
+            newArr.push("even index")
+        }
+        else {
+            newArr.push(anArray[i])
+        }
+    }
+    return newArr
+}
+console.log(replaceEvens(givenArr))
